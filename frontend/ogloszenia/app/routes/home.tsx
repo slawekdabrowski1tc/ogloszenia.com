39207cc3a53bb29d5a248a1842bff598c7ccdc13
Sławomir Dąbrowski <slawomir.dabrowski@tm1.edu.pl>
2025-09-18 11:56:39 +0200

    Added header.

--- frontend/ogloszenia/app/routes/home.tsx
+++ frontend/ogloszenia/app/routes/home.tsx
@@ -1,14 +1,35 @@
+import { Button } from "~/components/button";
 import type { Route } from "./+types/home";
+import { Input } from "~/components/input";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
+const categories = [
+  "Motoryzacja",
+  "Nieruchomości",
+  "Praca",
+  "Elektronika",
+  "Moda",
+  "Dom i ogród",
+  "Sport",
+  "Muzyka",
+  "Usługi",
+  "Zwierzęta",
+  "Dla dzieci",
+  "Inne"]
 
 export default function Home() {
-  return( <div>
-    nowa aplikacja
-    </div>)
+
+
+  return (<div className="min-h-screen flex flex-col">
+    <header className="flex justify-between items-center p-6 shadow-md bg-white">
+      <h1 className="text-2xl font-bold text-blue-600">ogloszenia.com</h1>
+      <Button variant="outline">Logowanie</Button>
+    </header>
+  </div>
+  )
 }
