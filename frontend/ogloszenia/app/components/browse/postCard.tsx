5d2e6df9c01cbae381ab1dbfc6c1128a58a278f5
Sławomir Dąbrowski <slawomir.dabrowski@tm1.edu.pl>
2025-10-09 10:53:41 +0200

    Added browsing page.

---
+++ frontend/ogloszenia/app/components/browse/postCard.tsx
@@ -0,0 +1,24 @@
+interface PostCardProps {
+    image: string;
+    title: string;
+    price: string;
+    category: string;
+  }
+  
+  export function PostCard({ image, title, price, category }: PostCardProps) {
+    return (
+      <div className="bg-white rounded-2xl shadow hover:shadow-lg transition overflow-hidden">
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-48 object-cover"
+        />
+        <div className="p-4">
+          <h3 className="font-semibold text-lg mb-2">{title}</h3>
+          <p className="text-green-600 font-bold mb-2">{price}</p>
+          <p className="text-gray-400 text-sm">{category}</p>
+        </div>
+      </div>
+    );
+  }
+  
--- frontend/ogloszenia/app/routes.ts
+++ frontend/ogloszenia/app/routes.ts
@@ -3,5 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [index("routes/home.tsx"), route("/login","routes/login.tsx"),
      route("/register", "routes/register.tsx"),
      route("/add", "routes/createPost.tsx"),
-     route("/p/:id", "routes/post.tsx")
+     route("/p/:id", "routes/post.tsx"),
+     route("/browse", "routes/browsePosts.tsx")
     ] satisfies RouteConfig;
